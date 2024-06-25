fe6857a74705611f70098707ba9b495da6054c64
asifazad799 <asifazad799@gmail.com>
2024-06-25 11:00:07 +0530

    added vitual fields in post schema

--- models/post.js
+++ models/post.js
@@ -1,12 +1,31 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const ratingSchema = new mongoose.Schema(
+    {
+        totalRatings: { type: Number, default: 0 },
+        totalUsersRated: { type: Number, default: 0 }
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+ratingSchema.virtual('average').get(function () {
+    if (this.totalUsersRated === 0) {
+        return 0;
+    }
+    return this.totalRatings / this.totalUsersRated;
+});
+
 const postSchema = new mongoose.Schema(
     {
         image: { type: [String], },
         title: { type: String },
         desc: { type: String },
-        auther: { type: Number, required: true }
+        auther: { type: Number, required: true }, // this will be changed with userId (ObjectId)
+        rating: { type: ratingSchema }
     },
     { timestamps: true }
 );
